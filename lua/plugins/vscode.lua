c1917e885cb822ba3b153fc6f6dd64ebcb7f954e
Thomas Müller <mut9ba@bosch.com>
2024-06-21 11:35:31 +0200

    Add shortcut for quick fix menu

--- lua/plugins/vscode.lua
+++ lua/plugins/vscode.lua
@@ -1,6 +1,7 @@
 return {
     vim.keymap.set("n", "<leader>ff", "<Cmd>call VSCodeNotify('workbench.action.findInFiles')<CR>"),
     vim.keymap.set("n", "<leader>fs", "<Cmd>call VSCodeNotify('workbench.action.findInFiles')<CR>"),
+    vim.keymap.set("n", "ga", "<Cmd>call VSCodeNotify('editor.action.quickFix')<CR>"),
     vim.keymap.set("n", "gi", "<Cmd>call VSCodeNotify('editor.action.goToImplementation')<CR>"),
     vim.keymap.set("n", "gl", "<Cmd>call VSCodeNotify('editor.action.marker.next')<CR>"),
     vim.keymap.set("n", "go", "<Cmd>call VSCodeNotify('editor.action.peekTypeDefinition')<CR>"),
