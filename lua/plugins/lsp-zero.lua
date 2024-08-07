317ac1e35a5838378d5aa8233be0a66e11ccca6d
Thomas Müller <mut9ba@bosch.com>
2024-08-07 11:16:24 +0200

    Add missing completion dependencies

--- lua/plugins/lsp-zero.lua
+++ lua/plugins/lsp-zero.lua
@@ -20,7 +20,14 @@ return {
         "hrsh7th/nvim-cmp",
         event = "InsertEnter",
         dependencies = {
-            { "L3MON4D3/LuaSnip" },
+            {
+                "L3MON4D3/LuaSnip",
+                "hrsh7th/cmp-buffer",
+                "hrsh7th/cmp-path",
+                "hrsh7th/cmp-nvim-lsp",
+                "hrsh7th/cmp-cmdline",
+                "hrsh7th/nvim-cmp"
+            },
         },
         config = function()
             -- Here is where you configure the autocompletion settings.
