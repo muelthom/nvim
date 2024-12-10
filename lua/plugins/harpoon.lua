657d63abf04e8cfa674225448868262102f3dfbb
Thomas Müller <mail@muellethomas.net>
2024-12-10 13:16:09 +0100

    Fix typo

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -5,39 +5,15 @@ return {
         lazy = false,
         requires = {
             "nvim-lua/plenary.nvim",
-            -- "nvim-telescope/telescope.nvim"
         },
         config = function()
             require("harpoon").setup({})
-
-            -- local function toggle_telescope_with_harpoon(harpoon_files)
-            --     local file_paths = {}
-            --     for _, item in ipairs(harpoon_files.items) do
-            --         table.insert(file_paths, item.value)
-            --     end
-            --
-            --     require("telescope.pickers")
-            --         .new({}, {
-            --             prompt_title = "Harpoon",
-            --             finder = require("telescope.finders").new_table({
-            --                 results = file_paths,
-            --             }),
-            --             previewer = require("telescope.config").values.file_previewer({}),
-            --             sorter = require("telescope.config").values.generic_sorter({}),
-            --         })
-            --         :find()
-            -- end
-            --
-            -- vim.keymap.set("n", "<leader>a", function()
-            --     local harpoon = require("harpoon")
-            --     toggle_telescope_with_harpoon(harpoon:list())
-            -- end, { desc = "Open harpoon window" })
         end,
         keys = {
             {
                 "<leader>a",
                 function() require("harpoon"):list():add() end,
-                desc = "Harpoon currtent buffer",
+                desc = "Harpoon current buffer",
             },
             {
                 "<C-b>",
