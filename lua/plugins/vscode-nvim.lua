d101ca453df9ea83f075acc3a98c2f83b8755bc6
Thomas Müller <mail@muellethomas.net>
2024-12-12 10:37:21 +0100

    Overwrite Python docstring color

--- lua/plugins/vscode-nvim.lua
+++ lua/plugins/vscode-nvim.lua
@@ -1,7 +1,12 @@
 return {
     "mofiqul/vscode.nvim",
     config = function()
-        require('vscode').setup({})
+        local c = require("vscode.colors").get_colors()
+        require("vscode").setup({
+            group_overrides = {
+                ["@string.documentation.python"] = { fg="#3a5240", bg=c.vscNone },
+            }
+        })
         vim.cmd.colorscheme "vscode"
     end,
 }
