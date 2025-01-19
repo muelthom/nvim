34e588f40af38883f81d463e315fa60ccf8e4ed9
Thomas Müller <mail@muellethomas.net>
2025-01-19 13:18:51 +0100

    Update keymaps

--- lua/remaps.lua
+++ lua/remaps.lua
@@ -37,21 +37,21 @@ vim.keymap.set("n", "<leader>Y", "\"+y")
 -- Unmap Q.
 vim.keymap.set("n", "Q", "<nop>")
 
+vim.keymap.set("c", "w!!", "w !sudo tee > /dev/null %", { noremap = true, silent = true })
+vim.keymap.set("c", "x!!", "w !chmod +x %", { noremap = true, silent = true })
+
 -- Turn off highlighting until the next search.
-vim.keymap.set("n", "<Esc>", ":noh<CR>")
+vim.keymap.set("n", "<ESC>", "v:hlsearch ? ':nohl<CR>' : '<ESC>'", { expr = true, silent = true })
 
 -- Select the whole file contents ("select all").
 vim.keymap.set("n", "<leader>sa", "ggVG")
 
--- Format code.
-vim.keymap.set("n", "<leader>fc", "ggVG= :echomsg 'Formatted code.'<CR>")
-
 -- Search and replace the word under the cursor.
 vim.keymap.set("n", "<leader>s", ":%s/\\<<C-r><C-w>\\>/<C-r><C-w>/gI<Left><Left><Left>")
 
--- Cyle through open buffers (left, right).
-vim.keymap.set({ "n", "v" }, "<C-j>", ":bnext<CR>")
-vim.keymap.set({ "n", "v" }, "<C-k>", ":bprev<CR>")
+-- Cycle through open buffers (left, right).
+-- vim.keymap.set({ "n", "v" }, "<C-j>", ":bnext<CR>")
+-- vim.keymap.set({ "n", "v" }, "<C-k>", ":bprev<CR>")
 
 -- Vertical equivalent of "<C-w>n" (create a new horizontal split with an empty buffer).
 vim.keymap.set({ "n", "v", "i" }, "<C-w>N", ":vnew<CR>")
@@ -62,12 +62,31 @@ vim.keymap.set({ "n", "v" }, "<leader>d", ":bnext<CR>")
 
 vim.api.nvim_set_keymap("t", "<C-t>", "<C-\\><C-n>:CFloatTerm<CR>", { noremap = true, silent = true })
 
--- Use "cwdf" for "change working directory to currently opened file".
-vim.api.nvim_create_user_command("CWDF",
-    function(args)
+vim.api.nvim_create_user_command("Cwdf",
+    function()
         vim.cmd(":cd %:h")
     end,
     { desc = "Change cwd to that of the currently opened file." }
 )
 
 vim.api.nvim_set_keymap("n", "!", ":lua invert_word()<CR>", { noremap = true, silent = true })
+
+vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
+
+vim.keymap.set("n", "<leader>rl", function()
+    vim.o.relativenumber = not vim.o.relativenumber
+    if vim.o.relativenumber then
+        vim.notify("Set relative line numbers", vim.log.levels.INFO, { title = "Relative Line Numbers" })
+    else
+        vim.notify("Unset relative line numbers", vim.log.levels.INFO, { title = "Relative Line Numbers" })
+    end
+end, { desc = "Toggle Relative Number", silent = true })
+
+vim.keymap.set("n", "<leader>ww", function()
+    vim.wo.wrap = not vim.wo.wrap
+    if vim.wo.wrap then
+        vim.notify("Set word wrap", vim.log.levels.INFO, { title = "Line Wrap" })
+    else
+        vim.notify("Unset word wrap", vim.log.levels.INFO, { title = "Line Wrap" })
+    end
+end, { desc = "Toggle Wrap", silent = true })
