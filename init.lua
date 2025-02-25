a098ff458c48a08e4a0e9759eab0513f3d030d64
Thomas Müller <mail@muellethomas.net>
2025-02-25 18:26:32 +0100

    Remove VS config for now

--- init.lua
+++ init.lua
@@ -1,37 +1,27 @@
-if vim.g.vscode then
-    -- VS Code extension
-    require("remaps")
-    require("plugins.vscode")
-    require("functions")
-else
-    -- Ordinary Neovim
+-- Custom keybindings
+require("remaps")
 
-    -- Custom keybindings
-    require("remaps")
+-- Global editor settings and functions
+require("settings")
+require("functions")
 
-    -- Editor settings
-    require("settings")
-    require("functions")
-
-    -- Configure plugins
-    -- Bootstrap lazy if it is not installed yet.
-    local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-    if not vim.loop.fs_stat(lazypath) then
-        vim.fn.system({
-            "git",
-            "clone",
-            "--filter=blob:none",
-            "https://github.com/folke/lazy.nvim.git",
-            "--branch=stable", -- latest stable release
-            lazypath,
-        })
-    end
-    vim.opt.rtp:prepend(lazypath)
-
-    -- Load Lua plugins from the plugins/ directory.
-    require("lazy").setup("plugins")
-
-    -- Global options (maybe be moved in the future)
-    -- Register the "nice-references" plugin to handle LSP "references" calls.
-    vim.lsp.handlers["textDocument/references"] = require "nice-reference".reference_handler
+-- Bootstrap lazy if it is not installed yet.
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
 end
+vim.opt.rtp:prepend(lazypath)
+
+-- Load Lua plugins from the plugins/ directory.
+require("lazy").setup("plugins")
+
+-- Global options (maybe be moved in the future)
+-- Register the "nice-references" plugin to handle LSP "references" calls.
+vim.lsp.handlers["textDocument/references"] = require "nice-reference".reference_handler
