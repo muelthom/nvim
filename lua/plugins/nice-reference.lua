d13f112283f3e37048735a46573abf0bcd6948de
Thomas Müller <mail@muellethomas.net>
2025-03-31 22:46:48 +0200

    Fix floating reference plugin config

--- lua/plugins/nice-reference.lua
+++ lua/plugins/nice-reference.lua
@@ -4,9 +4,9 @@ return {
         "nvim-tree/nvim-web-devicons", -- Optional
         {
             "rmagatti/goto-preview",
-            dependencies = { "rmagatti/logger.nvim" },
-            event = "BufEnter",
-            config = true, -- necessary as per https://github.com/rmagatti/goto-preview/issues/88
+            config = function()
+                require("goto-preview").setup({})
+            end,
         },
     },
 }
