73c8ed61c6331dc80428b4b3fdeea59deb5a68b1
Thomas Müller <mail@muellethomas.net>
2025-06-06 18:07:09 +0200

    Configure LSP to use 88 line length limit

--- lsp/pylsp.lua
+++ lsp/pylsp.lua
@@ -2,6 +2,18 @@ return {
     cmd = { "pylsp" },
     filetypes = { "python" },
     root_markers = { "pyproject.toml", "setup.py", "setup.cfg", "requirements.txt" },
+    single_file_support = true,
     -- Config: https://github.com/neovim/nvim-lspconfig/blob/master/doc/configs.md#pylsp
-    -- settings = { ... },
+    settings = {
+        pylsp = {
+            plugins = {
+                configurationSources = { "pycodestyle", "pylint" },
+                pycodestyle = {
+                    enabled = true,
+                    maxLineLength = 88,
+                },
+                pylint = { enabled = true },
+            },
+        },
+    },
 }
